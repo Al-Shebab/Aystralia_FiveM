a088c219683870ca45b7f1cc75a1b501453a2f35
Al Shebab <70445073+alshebabicus@users.noreply.github.com>
2021-02-10 20:02:22 +1100

    discord whitelist ect

---
+++ server-data/resources/[discord]/discord_perms/config.lua
@@ -0,0 +1,10 @@
+Config = {
+	DiscordToken = "NzU1NzkyOTI3ODkxOTE0ODEy.X2IdAw.STHAvVoEejnCD7e-sxdXX9uScsA",
+	GuildId = "749796716122865764",
+
+	Roles = {		
+		["Founder"] = "749796821840298034",
+		["Testing Team"] = "750208239005859840" ,-- This would be checked by doing exports.discord_perms:IsRolePresent(user, "TestRole")
+		["Server Developer"] = "752348563752616017" -- This would be checked by doing exports.discord_perms:IsRolePresent(user, "TestRole")
+	}
+}
