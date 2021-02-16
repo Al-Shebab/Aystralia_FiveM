8a1a65380c455110878ee0c661f67cca1252a330
Al Shebab <70445073+alshebabicus@users.noreply.github.com>
2021-02-16 09:46:02 -0800

    fix simeons window bruh

--- server-data/resources/[fixes]/loadipl/client.lua
+++ server-data/resources/[fixes]/loadipl/client.lua
@@ -2,11 +2,11 @@ Citizen.CreateThread(function()
 --load unloaded ipl's
   LoadMpDlcMaps()
   EnableMpDlcMaps(true)
-  RequestIpl("CS3_05_water_grp1")
-  RequestIpl("CS3_05_water_grp2")
-  RequestIpl("canyonriver01")
-  RequestIpl("railing_start")
-  
+  RequestIpl("CS3_05_water_grp1") -- Water Patch
+  RequestIpl("CS3_05_water_grp2") -- Water Patch
+  RequestIpl("canyonriver01") -- Water Patch
+  RequestIpl("railing_start") -- Honestly no clue
+  RequestIpl("shr_int") -- Simeons
 end)
 
 -- http://pastebin.com/FyV5mMma for list if you want to add more
