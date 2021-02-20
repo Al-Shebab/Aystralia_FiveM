816c6affe619f80b7bc6f28301d7978c7937ab0a
Al Shebab <70445073+alshebabicus@users.noreply.github.com>
2021-02-20 20:26:39 +1100

    fix fucky wucky on emote menu

--- server-data/resources/[player]/dpemotes/Config.lua
+++ server-data/resources/[player]/dpemotes/Config.lua
@@ -10,7 +10,7 @@ Config = {
 -- Set this to true if you want to disarm the player when they play an emote.
 	DisarmPlayer= false,
 -- Set this if you really wanna disable emotes in cars, as of 1.7.2 they only play the upper body part if in vehicle
-    AllowedInCars = false,
+    AllowedInCars = true,
 -- You can disable the (F3) menu here / change the keybind.
 	MenuKeybindEnabled = true,
 	MenuKeybind = 170, -- Get the button number here https://docs.fivem.net/game-references/controls/
@@ -49,7 +49,7 @@ Config.KeybindKeys = {
 
 Config.Languages = {
   ['en'] = {
-        ['emotes'] = '',
+        ['emotes'] = 'Emotes',
         ['danceemotes'] = "🕺 Dance Emotes",
         ['propemotes'] = "📦 Prop Emotes",
         ['favoriteemotes'] = "🌟 Favorite",
