86369fbe13e0305c3b8c223b6401579d1a1177df
Al Shebab <70445073+alshebabicus@users.noreply.github.com>
2021-02-23 05:10:08 -0800

    add showroom display

--- server-data/resources/[stores]/vrp_showroom/cars/config.lua
+++ server-data/resources/[stores]/vrp_showroom/cars/config.lua
@@ -8,25 +8,25 @@ Cars = {
     {
         pos = vec(-43.2, -1094.29, 26.41),
         heading = 114.57,
-        model = 'bugattichiron',
+        model = 'adder',
         spin = false,
         text = "",
-        plate = "ELRP"
+        plate = "Ay"
     },
     {
         pos = vec(-48.3, -1092.41, 26.41),
         heading = 114.57,
-        model = 'devcar',
+        model = 'adder',
         spin = false,
         text = "",
-        plate = "ELRP"
+        plate = "Ay"
     },
     {
         pos = vec(-37.5, -1088.17, 26.43),
         heading = 250.05,
-        model = 'senna',
+        model = 'adder',
         spin = false,
         text = "",
-        plate = "ELRP"
+        plate = "Ay"
     },
 }
