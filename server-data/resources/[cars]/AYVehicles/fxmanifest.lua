a4bfa9f007f1f5e0e0f05c0974a1c6de9c96129f
Al Shebab <70445073+alshebabicus@users.noreply.github.com>
2021-02-24 14:42:05 +1100

    dealership cars

---
+++ server-data/resources/[cars]/AYVehicles/fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author 'Blaze'
+description 'All Elite RP Vehicles'
+
+files {
+	'data/**/vehicles.meta',
+	'data/**/carvariations.meta',
+	'data/**/handling.meta',
+	'data/**/vehiclelayouts.meta',
+	'data/**/dlctext.meta',
+	'data/**/370zcontentunlocks.meta',
+	'data/**/rmodrs7contentunlocks.meta',
+	'data/**/16challengercontentunlocks.meta',
+	'data/**/vehiclelayouts_rumpoesc.meta'
+}
+
+data_file 'handling_file' 					'data/**/*handling.meta'
+data_file 'vehicle_metadata_file' 			'data/**/*vehicles.meta'
+data_file 'dlctext_file'					'data/**/*dlctext.meta'
+data_file 'carcols_file'					'data/**/*carcols.meta'
+data_file 'vehicle_variation_file'			'data/**/*carvariations.meta'
+data_file 'vehicle_layouts_file'			'data/**/*vehicleslayouts.meta'
