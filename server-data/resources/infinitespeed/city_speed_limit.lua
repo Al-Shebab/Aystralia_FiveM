cd498375c7ed6b2776c8f57491a29caf5fd0c8b1
alshebabicus <frankthesexgod@gmail.com>
2021-06-20 19:20:40 +1000

    infinite speed

---
+++ server-data/resources/infinitespeed/city_speed_limit.lua
@@ -0,0 +1,39 @@
+local blips = {
+
+    {title="City Speed Limit", colour=62, id=378, x = 291.50555419922, y = -857.20031738281, z = 29.216951370239},
+    }
+    Citizen.CreateThread(function()
+    
+            Citizen.Wait(0)
+    
+    local bool = true
+      
+      if bool then
+            
+            for k,v in pairs(blips) do
+               
+    
+                   zoneblip = AddBlipForRadius(v.x,v.y,v.z, 300.0)
+                              SetBlipSprite(zoneblip,4 * 3.14159)
+                              SetBlipColour(zoneblip,62)
+                              SetBlipAlpha(zoneblip,30)
+                             
+            end
+             
+        
+             for _, info in pairs(blips) do
+            
+                 info.blip = AddBlipForCoord(info.x, info.y, info.z)
+                             SetBlipSprite(info.blip, info.id)
+                             SetBlipDisplay(info.blip, 1)
+                             SetBlipColour(info.blip, info.colour)
+                             SetBlipAsShortRange(info.blip, true)
+                             BeginTextCommandSetBlipName("STRING")
+                             AddTextComponentString(info.title)
+                             EndTextCommandSetBlipName(info.blip)
+             end
+           
+           bool = false
+       
+       end
+    end)
--- start.bat
+++ start.bat
@@ -1,2 +1,3 @@
 @echo off
-C:/Users/kazza/Desktop/Development/server//FXServer.exe +set svgui_disable true +set serverProfile "default"
+C:/Users/kazza/Desktop/Aystralia/FiveM/server/FXServer.exe +set svgui_disable true +set serverProfile "default"
+pause
