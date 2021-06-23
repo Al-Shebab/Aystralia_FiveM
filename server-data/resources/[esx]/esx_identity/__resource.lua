2cbe70a3d5b5c270970c029a2331afad7e393642
Mailar <83369389+MailarYT@users.noreply.github.com>
2021-06-23 12:54:34 +0300

    changes

--- server-data/resources/[esx]/esx_identity/__resource.lua
+++ server-data/resources/[esx]/esx_identity/__resource.lua
@@ -1,25 +1,37 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
-description 'ESX Identity'
+ui_page 'html/index.html'
 
-version '4.0.5'
+resource_type 'map' { gameTypes = { fivem = true } }
 
-server_scripts {
-    '@mysql-async/lib/MySQL.lua',
-    '@es_extended/locale.lua',
-    'locales/en.lua',
-    'config.lua',
-    'server/main.lua'
+map 'map.lua'
+
+server_script {
+	'@mysql-async/lib/MySQL.lua',
+	'server.lua'
 }
 
-client_scripts {
-    '@es_extended/locale.lua',
-    'locales/en.lua',
-    'config.lua',
-    'client/main.lua'
+client_script {
+	'client.lua'
 }
 
-dependencies {
-    'es_extended',
-    'esx_skin'
+files {
+	'html/assets/images/left.png',
+	'html/assets/images/right.png',
+	'html/index.html',
+	'html/assets/fonts/roboto/Roboto-Bold.woff',
+	'html/assets/fonts/roboto/Roboto-Bold.woff2',
+	'html/assets/fonts/roboto/Roboto-Light.woff',
+	'html/assets/fonts/roboto/Roboto-Light.woff2',
+	'html/assets/fonts/roboto/Roboto-Medium.woff',
+	'html/assets/fonts/roboto/Roboto-Medium.woff2',
+	'html/assets/fonts/roboto/Roboto-Regular.woff',
+	'html/assets/fonts/roboto/Roboto-Regular.woff2',
+	'html/assets/fonts/roboto/Roboto-Thin.woff',
+	'html/assets/fonts/roboto/Roboto-Thin.woff2',
+	'html/assets/css/materialize.css',
+	'html/assets/css/style.css',
+	'html/assets/js/jquery.js',
+	'html/assets/js/materialize.js',
+	'html/assets/js/init.js',
 }
