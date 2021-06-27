bc8f4ec13b24c327ee8075ae55fd20b778332d22
Mailar <83369389+MailarYT@users.noreply.github.com>
2021-06-27 11:43:31 +0300

    scoreboard fixes pt 1

---
+++ server-data/resources/[hud]/AYScoreboard/config-sh.lua
@@ -0,0 +1,5 @@
+Config = {}
+
+Config.GuildId = ("775399964204990495")
+Config.UpdateInterval = 5 -- // Seconds!
+Config.ToggleKey = 212 -- // (https://docs.fivem.net/docs/game-references/controls/)
