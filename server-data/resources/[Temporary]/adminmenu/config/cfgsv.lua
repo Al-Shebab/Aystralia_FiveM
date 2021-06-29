e0d3087efc4f7ef0541107825b215f18ccf38c4d
Mailar <83369389+MailarYT@users.noreply.github.com>
2021-06-29 20:04:10 +0300

    test

---
+++ server-data/resources/[Temporary]/adminmenu/config/cfgsv.lua
@@ -0,0 +1,23 @@
+ConfigSV = {}
+
+ConfigSV.KickBanWebhook = "https://discord.com/api/webhooks/859044724987658241/wlPqCa9NwmMxTvcskXm6G4mNjk8Iu_4P8AkNgZzo5uAQil8BECb1xhhcSf-gVnh3fFI9"
+
+ConfigSV.ScreenshotLogsWebhook = "https://discord.com/api/webhooks/859044724987658241/wlPqCa9NwmMxTvcskXm6G4mNjk8Iu_4P8AkNgZzo5uAQil8BECb1xhhcSf-gVnh3fFI9"
+
+ConfigSV.ScreenshotsWebhook = "https://discord.com/api/webhooks/859044724987658241/wlPqCa9NwmMxTvcskXm6G4mNjk8Iu_4P8AkNgZzo5uAQil8BECb1xhhcSf-gVnh3fFI9"
+
+ConfigSV.ReviveWebhook = "https://discord.com/api/webhooks/859044724987658241/wlPqCa9NwmMxTvcskXm6G4mNjk8Iu_4P8AkNgZzo5uAQil8BECb1xhhcSf-gVnh3fFI9"
+
+ConfigSV.MoneyWebhook = "https://discord.com/api/webhooks/859044724987658241/wlPqCa9NwmMxTvcskXm6G4mNjk8Iu_4P8AkNgZzo5uAQil8BECb1xhhcSf-gVnh3fFI9"
+
+ConfigSV.SlapWebhook = "https://discord.com/api/webhooks/859044724987658241/wlPqCa9NwmMxTvcskXm6G4mNjk8Iu_4P8AkNgZzo5uAQil8BECb1xhhcSf-gVnh3fFI9"
+
+ConfigSV.ForceskinWebhook = "https://discord.com/api/webhooks/859044724987658241/wlPqCa9NwmMxTvcskXm6G4mNjk8Iu_4P8AkNgZzo5uAQil8BECb1xhhcSf-gVnh3fFI9"
+
+ConfigSV.ClearloadoutWebhook = "https://discord.com/api/webhooks/859044724987658241/wlPqCa9NwmMxTvcskXm6G4mNjk8Iu_4P8AkNgZzo5uAQil8BECb1xhhcSf-gVnh3fFI9"
+
+ConfigSV.FreezeWebhook = "https://discord.com/api/webhooks/859044724987658241/wlPqCa9NwmMxTvcskXm6G4mNjk8Iu_4P8AkNgZzo5uAQil8BECb1xhhcSf-gVnh3fFI9"
+
+ConfigSV.Discord = ""
+
+ConfigSV.ServerName = "Heartless RP"
