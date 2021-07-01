311c7550d5dc2f967c8dbe06a8558bf1a5b7ece3
Mailar <83369389+MailarYT@users.noreply.github.com>
2021-07-01 14:49:14 +0300

    add

---
+++ server-data/resources/AYHolograms/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+games { 'gta5' }
+author 'Aystralia'
+
+client_scripts {
+    'holograms.lua'
+}
+
+server_scripts {
+
+}
---
+++ server-data/resources/AYHolograms/holograms.lua
@@ -0,0 +1,6 @@
+CreateThread(function()
+	while true do
+		Wait(0)
+        DrawMarker(36, -55.68, -1096.61, 43.22+2, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 20.75, 20.75, 20.75, 0, 255, 0, 200, true, true, 2, true, false, false, false)
+	end
+end)
