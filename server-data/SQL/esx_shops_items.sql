b334f72ed050bbdd88b1d8b0038c68a5ae00031d
Mailar <83369389+MailarYT@users.noreply.github.com>
2021-07-06 12:25:39 +0300

    sql items file for esx_shops

---
+++ server-data/SQL/esx_shops_items.sql
@@ -0,0 +1,15 @@
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
+	('water', 'Water', 1, 0, 1),
+	('bread', 'Bread', 1, 0, 1),
+	('cola', 'Coca Cola', 1, 0, 1),
+	('hamburger', 'Big Mac', 1, 0, 1),
+	('energetic', 'Energy Drink', 1, 0, 1),
+	('cupcake', 'Cup Cake', 1, 0, 1),
+	('beer', 'Beer', 1, 0, 1),
+	('wytrych', 'Wytrych', 1, 0, 1),
+	('lighter', 'Lighter', 1, 0, 1),
+	('cigarette', 'Cigarette', 1, 0, 1),
+	('radio', 'Radio', 1, 0, 1),
+	('phone', 'Samsung S9', 1, 0, 1),
+	('handcuffs', 'Handcuffs', 1, 0, 1)
+;
