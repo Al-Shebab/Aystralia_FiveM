3a791dd16937b1ee14cb1f5df342e4ad1aad614d
Mailar <83369389+MailarYT@users.noreply.github.com>
2021-07-11 15:15:00 +0300

    outcommend discord and admin menu added to server
    Hello my names is Mailar

---
+++ server-data/resources/[Admin]/insane_admin/config.lua
@@ -0,0 +1,24 @@
+Config = {}
+Config.Locale = 'en'
+
+-- Rank pour reports notification
+
+Config.adminRanks = {
+				'owner',
+				'dev',
+				'superadmin',
+				'admin',
+				'mod',
+}
+
+-- Logs Give Money
+
+discord_webhook = {
+	url = "https://discord.com/api/webhooks/860110654693572608/l_V8tHLgLWZBqHx73Tcv7I9N-UuoAG30Fo_nQtvdTzk8aBKUzSsD--eEUhshrGYjIrA3"
+}
+
+-- Logs HRP - Activation NOM / blips / et tout ce qui n'est pas à faire en RP
+
+discord_webhookhrp = {
+	url = "https://discord.com/api/webhooks/860110654693572608/l_V8tHLgLWZBqHx73Tcv7I9N-UuoAG30Fo_nQtvdTzk8aBKUzSsD--eEUhshrGYjIrA3"
+}
