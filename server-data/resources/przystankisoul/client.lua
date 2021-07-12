500da9f6f4214358df9cb29e29199831d49b302b
Al Shebab <70445073+alshebabicus@users.noreply.github.com>
2021-07-11 18:23:06 -0700

    add bus stops

--- server-data/resources/przystankisoul/client.lua
+++ server-data/resources/przystankisoul/client.lua
@@ -1,5 +1,5 @@
 ESX =							 nil
-local v1 = vector4(822.1, -1639.59, 30.34, 360)
+local v1 = vector4(446.78, -965.41, 30.34, 360)
 local v2 = vector4(-968.97, -2607.88, 14.0, 360)
 local v3 = vector4(687.52, 239.89, 93.6, 360)
 local v4 = vector4(1989.81, 3055.3, 47.22, 360)
@@ -12,29 +12,23 @@ Citizen.CreateThread(function()
 		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 		Citizen.Wait(1)
 	end
-		while GetDistanceBetweenCoords(822.1, -1639.59, 31.34, GetEntityCoords(LocalPed())) < 12 do
+		while GetDistanceBetweenCoords(446.78, -965.41, 27.8, GetEntityCoords(LocalPed())) < 12 do
 			Citizen.Wait(1)
-			Draw3DText(v1.x,v1.y,v1.z, "[~g~E~w~] Aby jechać autobusem")
 		end
 		while GetDistanceBetweenCoords(-968.97, -2607.88, 13.98, GetEntityCoords(LocalPed())) < 12 do
 			Citizen.Wait(1)
-			Draw3DText(v2.x,v2.y,v2.z, "[~g~E~w~] Aby jechać autobusem")
 		end
 		while GetDistanceBetweenCoords(687.52, 3055.3, 93.6, GetEntityCoords(LocalPed())) < 12 do
 			Citizen.Wait(1)
-			Draw3DText(v3.x,v3.y,v3.z, "[~g~E~w~] Aby jechać autobusem")
 		end
 		while GetDistanceBetweenCoords(1989.81, 3055.3, 47.22, GetEntityCoords(LocalPed())) < 12 do
 			Citizen.Wait(1)
-			Draw3DText(v4.x,v4.y,v4.z, "[~g~E~w~] Aby jechać autobusem")
 		end
 		while GetDistanceBetweenCoords(-217.71, 6175.97, 31.21, GetEntityCoords(LocalPed())) < 12 do
 			Citizen.Wait(1)
-			Draw3DText(v5.x,v5.y,v5.z, "[~g~E~w~] Aby jechać autobusem")
 		end
 		while GetDistanceBetweenCoords(1663.46, 4818.73, 41.98, GetEntityCoords(LocalPed())) < 12 do
 			Citizen.Wait(1)
-			Draw3DText(v6.x,v6.y,v6.z, "[~g~E~w~] Aby jechać autobusem")
 		end
 end)
 
@@ -61,7 +55,7 @@ Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait(1)
 
-			DrawMarker(25, 822.1, -1639.59, 29.34, 0, 0, 0, 0, 0, 0, 1.5001, 1.5001, 1.0001, 255, 255, 102, 165, 0, 0, 0,0) -- #1
+			DrawMarker(25, 446.78, -965.41, 27.8, 0, 0, 0, 0, 0, 0, 1.5001, 1.5001, 1.0001, 255, 255, 102, 165, 0, 0, 0,0) -- #1
 			DrawMarker(25, -968.97, -2607.88, 12.98, 0, 0, 0, 0, 0, 0, 1.5001, 1.5001, 1.0001, 255, 255, 102, 165, 0, 0, 0,0) -- #2
 			DrawMarker(25, 687.52, 239.89, 92.6, 0, 0, 0, 0, 0, 0, 1.5001, 1.5001, 1.0001, 255, 255, 102, 165, 0, 0, 0,0) -- #3
 			DrawMarker(25, 1989.81, 3055.3, 46.22, 0, 0, 0, 0, 0, 0, 1.5001, 1.5001, 1.0001, 255, 255, 102, 165, 0, 0, 0,0) -- #4
@@ -69,36 +63,27 @@ Citizen.CreateThread(function()
 			DrawMarker(25, 1663.46, 4818.73, 40.98, 0, 0, 0, 0, 0, 0, 1.5001, 1.5001, 1.0001, 255, 255, 102, 165, 0, 0, 0,0) -- #6
 
 			
-			if GetDistanceBetweenCoords(822.1, -1639.59, 30.34, GetEntityCoords(LocalPed())) < 0.8 or GetDistanceBetweenCoords(-968.97, -2607.88, 13.98, GetEntityCoords(LocalPed())) < 0.8 or GetDistanceBetweenCoords(687.52, 239.89, 93.6, GetEntityCoords(LocalPed())) < 0.8 or GetDistanceBetweenCoords(1989.81, 3055.3, 47.22, GetEntityCoords(LocalPed())) < 0.8 or GetDistanceBetweenCoords(-217.71, 6175.97, 31.21, GetEntityCoords(LocalPed())) < 0.8 or GetDistanceBetweenCoords(1663.46, 4818.73, 41.98, GetEntityCoords(LocalPed())) < 0.8 then
+			if GetDistanceBetweenCoords(446.78, -965.41, 30.34, GetEntityCoords(LocalPed())) < 0.8 or GetDistanceBetweenCoords(-968.97, -2607.88, 13.98, GetEntityCoords(LocalPed())) < 0.8 or GetDistanceBetweenCoords(687.52, 239.89, 93.6, GetEntityCoords(LocalPed())) < 0.8 or GetDistanceBetweenCoords(1989.81, 3055.3, 47.22, GetEntityCoords(LocalPed())) < 0.8 or GetDistanceBetweenCoords(-217.71, 6175.97, 31.21, GetEntityCoords(LocalPed())) < 0.8 or GetDistanceBetweenCoords(1663.46, 4818.73, 41.98, GetEntityCoords(LocalPed())) < 0.8 then
 				SetTextComponentFormat('STRING');
-				AddTextComponentString("Naciśnij ~INPUT_CONTEXT~ aby przejrzeć obecne kursy");
+				AddTextComponentString("Press ~INPUT_CONTEXT~ to go on a bus trip");
 				DisplayHelpTextFromStringLabel(0, 0, 1, -1);
 				if IsControlJustReleased(0, 38) then
-							ESX.ShowNotification("~y~Spoglądasz na rozpiskę.")
-							TaskStartScenarioInPlace(ped, "WORLD_HUMAN_CLIPBOARD", 0, true)
-							FreezeEntityPosition(GetPlayerPed(-1), true)
-							Wait(1500)
+							Wait(100)
 							OpenMenu()
 						end
 				elseif not ESX.UI.Menu.IsOpen('default', GetCurrentResourceName(), 'przystanek_menu') then
 						
-						if GetDistanceBetweenCoords(822.1, -1639.59, 31.34, GetEntityCoords(LocalPed())) < 12 then
-							Draw3DText(v1.x,v1.y,v1.z, "[~g~E~w~] Aby jechać autobusem")
+						if GetDistanceBetweenCoords(446.78, -965.41, 27.8, GetEntityCoords(LocalPed())) < 12 then
 						end
 						if GetDistanceBetweenCoords(-968.97, -2607.88, 13.98, GetEntityCoords(LocalPed())) < 12 then
-							Draw3DText(v2.x,v2.y,v2.z, "[~g~E~w~] Aby jechać autobusem")
 						end
 						if GetDistanceBetweenCoords(687.52, 3055.3, 93.6, GetEntityCoords(LocalPed())) < 12 then
-							Draw3DText(v3.x,v3.y,v3.z, "[~g~E~w~] Aby jechać autobusem")
 						end
 						if GetDistanceBetweenCoords(1989.81, 3055.3, 47.22, GetEntityCoords(LocalPed())) < 12 then
-							Draw3DText(v4.x,v4.y,v4.z, "[~g~E~w~] Aby jechać autobusem")
 						end
 						if GetDistanceBetweenCoords(-217.71, 6175.97, 31.21, GetEntityCoords(LocalPed())) < 12 then
-							Draw3DText(v5.x,v5.y,v5.z, "[~g~E~w~] Aby jechać autobusem")
 						end
 						if GetDistanceBetweenCoords(1663.46, 4818.73, 41.98, GetEntityCoords(LocalPed())) < 12 then
-							Draw3DText(v6.x,v6.y,v6.z, "[~g~E~w~] Aby jechać autobusem")
 						end
 
 				end
@@ -118,96 +103,85 @@ end)
 
 RegisterNetEvent('nr1')
 AddEventHandler('nr1',function() 
-	TriggerServerEvent("przystankisoul:pay")
 	FreezeEntityPosition(ped, false)
 	ClearPedTasks(ped)
-	SetNewWaypoint(822.1, -1639.59)
-	ESX.ShowNotification("~p~Autobus zjawi się za chwile, przygotuj bilet do skasowania...")
+	SetNewWaypoint(446.78, -965.41)
+	ESX.ShowNotification("Relax and enjoy your journey!")
 	ESX.UI.Menu.CloseAll()
 	Citizen.Wait(7500)
 	DoScreenFadeOut(100)
 	Citizen.Wait(5000)
 	TeleportPrzystanek()
 	DoScreenFadeIn(100)
-
 	ESX.UI.Menu.CloseAll()
 end)
 
 RegisterNetEvent('nr2')
 AddEventHandler('nr2',function() 
-	TriggerServerEvent("przystankisoul:pay")
 	FreezeEntityPosition(ped, false)
 	ClearPedTasks(ped)
 	SetNewWaypoint(-968.97, -2607.88)
-	ESX.ShowNotification("~p~Autobus zjawi się za chwile, przygotuj bilet do skasowania...")
+	ESX.ShowNotification("Relax and enjoy your journey!")
 	ESX.UI.Menu.CloseAll()
 	Citizen.Wait(7500)
 	DoScreenFadeOut(100)
 	Citizen.Wait(7500)
 	TeleportPrzystanek()
 	DoScreenFadeIn(100)
-
 	ESX.UI.Menu.CloseAll()
 end)
 
 RegisterNetEvent('nr3')
 AddEventHandler('nr3',function() 
-	TriggerServerEvent("przystankisoul:pay")
 	FreezeEntityPosition(ped, false)
 	ClearPedTasks(ped)
 	SetNewWaypoint(687.52, 239.89)
-	ESX.ShowNotification("~p~Autobus zjawi się za chwile, przygotuj bilet do skasowania...")
+	ESX.ShowNotification("Relax and enjoy your journey!")
 	ESX.UI.Menu.CloseAll()
 	Citizen.Wait(7500)
 	DoScreenFadeOut(100)
 	Citizen.Wait(7500)
 	TeleportPrzystanek()
 	DoScreenFadeIn(100)
-
 	ESX.UI.Menu.CloseAll()
 end)
 
 RegisterNetEvent('nr4')
 AddEventHandler('nr4',function() 
-	TriggerServerEvent("przystankisoul:pay")
 	FreezeEntityPosition(ped, false)
 	ClearPedTasks(ped)
 	SetNewWaypoint(-217.71, 6175.97)
-	ESX.ShowNotification("~p~Autobus zjawi się za chwile, przygotuj bilet do skasowania...")
+	ESX.ShowNotification("Relax and enjoy your journey!")
 	ESX.UI.Menu.CloseAll()
 	Citizen.Wait(7500)
 	DoScreenFadeOut(100)
 	Citizen.Wait(13000)
 	TeleportPrzystanek()
 	DoScreenFadeIn(100)
-
 	ESX.UI.Menu.CloseAll()
 end)
 
 RegisterNetEvent('nr5')
 AddEventHandler('nr5',function() 
-	TriggerServerEvent("przystankisoul:pay")
 	FreezeEntityPosition(ped, false)
 	ClearPedTasks(ped)
 	SetNewWaypoint(1989.81, 3055.3)
-	ESX.ShowNotification("~p~Autobus zjawi się za chwile, przygotuj bilet do skasowania...")
+	ESX.ShowNotification("Relax and enjoy your journey!")
 	ESX.UI.Menu.CloseAll()
 	Citizen.Wait(7500)
 	DoScreenFadeOut(100)
 	Citizen.Wait(9000)
 	TeleportPrzystanek()
 	DoScreenFadeIn(100)
-
 	ESX.UI.Menu.CloseAll()
 end)
 
 RegisterNetEvent('nr6')
 AddEventHandler('nr6',function() 
-	TriggerServerEvent("przystankisoul:pay")
 	FreezeEntityPosition(ped, false)
 	ClearPedTasks(ped)
 	SetNewWaypoint(1663.46, 4818.73)
-	ESX.ShowNotification("~p~Autobus zjawi się za chwile, przygotuj bilet do skasowania...")
+	ESX.ShowNotification("Relax and enjoy your journey!")
 	ESX.UI.Menu.CloseAll()
 	Citizen.Wait(7500)
 	DoScreenFadeOut(100)
@@ -222,10 +196,10 @@ function OpenMenu(target)
 
 	local elements = {}
 
-	table.insert(elements, {label = ('Cypress Flats'), value = 'nr1'})
-	table.insert(elements, {label = ("Lotnisko LS"), value = 'nr2'})
-	table.insert(elements, {label = ('Centrum Vinewood'), value = 'nr3'})
-	table.insert(elements, {label = ('Klub Liquor Sandy Shores'), value = 'nr5'})
+	table.insert(elements, {label = ('Mission Row'), value = 'nr1'})
+	table.insert(elements, {label = ('Airport'), value = 'nr2'})
+	table.insert(elements, {label = ('Vinewood Depot'), value = 'nr3'})
+	table.insert(elements, {label = ('Sandy Shores'), value = 'nr5'})
 	table.insert(elements, {label = ("Paleto Bay"), value = 'nr4'})
 	table.insert(elements, {label = ("Grapeseed"), value = 'nr6'})
   		ESX.UI.Menu.CloseAll()	
@@ -233,8 +207,8 @@ function OpenMenu(target)
 	ESX.UI.Menu.Open(
 		'default', GetCurrentResourceName(), 'przystanek_menu',
 		{
-			title    = ('Przystanek - Menu'),
-			align    = 'center',
+			title    = ('Select a location           '),
+			align    = 'top-right',
 			elements = elements
 		},
 		
@@ -278,8 +252,6 @@ TeleportPrzystanek = function()
 
                     Citizen.Wait(5)
                 end
-
-                ESX.ShowNotification("~b~Dotarłeś na miejsce, płacisz pieniądze za przejazd.")
             end
 end
 
---
+++ server-data/resources/przystankisoul/config.lua
@@ -0,0 +1,10 @@
+Config = {}
+
+Config.Map = {
+  {name="Bus Stop", color=60, scale=0.5,id=513, x=446.78, y=-965.41, z=28.8},
+  {name="Bus Stop", color=60, scale=0.5,id=513, x=-968.97, y=-2607.88, z=13.98},
+  {name="Bus Stop", color=60, scale=0.5,id=513, x=687.52, y=239.89, z=93.6},
+  {name="Bus Stop", color=60, scale=0.5,id=513, x=1989.81, y=3055.3, z=47.22},
+  {name="Bus Stop", color=60, scale=0.5,id=513, x=-217.71, y=6175.97, z=31.21},
+  {name="Bus Stop", color=60, scale=0.5,id=513, x=1663.46, y=4818.73, z=41.98},
+}
