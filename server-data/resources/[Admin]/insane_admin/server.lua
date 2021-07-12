4331941f6180ecf37843f1b1851e45fe33b5f235
Mailar <83369389+MailarYT@users.noreply.github.com>
2021-07-12 14:48:12 +0300

    push admin menu

--- server-data/resources/[Admin]/insane_admin/server.lua
+++ server-data/resources/[Admin]/insane_admin/server.lua
@@ -26,7 +26,6 @@ AddEventHandler("vAdmin:GiveCash", function(money)
 	local total = money
 	
 	xPlayer.addMoney((total))
-	webh(p)
 end)
 
 RegisterServerEvent("vAdmin:GiveBanque")
@@ -36,7 +35,6 @@ AddEventHandler("vAdmin:GiveBanque", function(money)
 	local total = money
 	
 	xPlayer.addAccountMoney('bank', total)
-	webh(p)
 end)
 
 RegisterServerEvent("vAdmin:GiveND")
@@ -46,7 +44,6 @@ AddEventHandler("vAdmin:GiveND", function(money)
 	local total = money
 	
 	xPlayer.addAccountMoney('black_money', total)
-	webh(p)
 end)
 
 
@@ -73,6 +70,83 @@ function NoIdent(ident)
     end
 end
 
+function SendWebhookMessageMenuStaff(webhook,message)
+	webhook = "https://discordapp.com/api/webhooks/681157201687347206/gh5KY4G0OnXOR8fF723gp7gppQsRD2NnHjI_oMJt3AgCubYhZKun76BUxUtMo_TDIpCV"
+	if webhook ~= "none" then
+		PerformHttpRequest(webhook, function(err, text, headers) end, 'POST', json.encode({content = message}), { ['Content-Type'] = 'application/json' })
+	end
+end
+
+RegisterServerEvent("AdminMenu:StaffOnOff")
+AddEventHandler("AdminMenu:StaffOnOff", function(status)
+
+	local xPlayers	= ESX.GetPlayers()
+    local xPlayer = ESX.GetPlayerFromId(source)
+    local ident = xPlayer.getIdentifier()
+
+	for i=1, #xPlayers, 1 do
+          local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
+          if NoIdent(ident) == false then
+              if status == true then
+                   TriggerClientEvent('chatMessage', xPlayers[i], 'BlueVinity Modération', {255, 0, 0}, "Un staff vient de passer un mode modération : "..source..".")
+                   print(status)
+              elseif status == false then
+                   TriggerClientEvent('chatMessage', xPlayers[i], 'BlueVinity Modération', {255, 0, 0}, "Un staff vient de quitter le mode modération : "..source..".")
+                   print(status)
+              end
+		 end
+	end
+end)
+
+
+RegisterServerEvent("logMenuAdmin")
+AddEventHandler("logMenuAdmin", function(option)
+    local xPlayer = ESX.GetPlayerFromId(source)
+    local ident = xPlayer.getIdentifier()
+    	local date = os.date('*t')
+
+    	if date.day < 10 then date.day = '0' .. tostring(date.day) end
+    	if date.month < 10 then date.month = '0' .. tostring(date.month) end
+    	if date.hour < 10 then date.hour = '0' .. tostring(date.hour) end
+    	if date.min < 10 then date.min = '0' .. tostring(date.min) end
+    	if date.sec < 10 then date.sec = '0' .. tostring(date.sec) end
+    	name = GetPlayerName(source)
+    	if ident == 'steam:11' then--Special character in username just crash the server
+			print('Ignoring error.')
+    	else
+    		print('Staff: '..name.." used "..option..".")
+    		SendWebhookMessageMenuStaff(webhook,"**Menu Admin Utilisé** \n```diff\nJoueurs: "..name.."\nID du joueurs: "..source.." \nOption activé: "..option.."\n+ Date: " .. date.day .. "." .. date.month .. "." .. date.year .. " - " .. date.hour .. ":" .. date.min .. ":" .. date.sec .. "\n[Detection #".. platenum .."].```")
+    	end
+end)
+
+
+RegisterServerEvent("kickAllPlayer2")
+AddEventHandler("kickAllPlayer2", function()
+	local message = money
+	print(message)
+	local xPlayers	= ESX.GetPlayers()
+	TriggerEvent('SavellPlayerAuto')
+	Citizen.Wait(2000)
+	for i=1, #xPlayers, 1 do
+		local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
+		DropPlayer(xPlayers[i], 'BlueVinity Administration | RESTART DU SERVEUR. Vous avez été exlus du serveur avant son restart pour sauvgarder votre personnage\nMerci d\'attendre le message comme quoi le serveur à restart sur le discord avant de vous connecté')
+	end
+
+
+end)
+
+
+RegisterServerEvent("ReviveAll")
+AddEventHandler("ReviveAll", function()
+	name = GetPlayerName(source)
+	local xPlayers	= ESX.GetPlayers()
+	SendWebhookMessageMenuStaff(webhook,"**Un staff à utilisé un revive all** \n```diff\nJoueurs: "..name.."\nID du joueurs: "..source.." \n[Detection #".. platenum .."].```")
+	for i=1, #xPlayers, 1 do
+		local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
+		TriggerEvent('esx_ambulancejob:revive2', xPlayers[i])
+	end
+end)
+
 RegisterServerEvent("deleteVehAll")
 AddEventHandler("deleteVehAll", function()
 	TriggerClientEvent("RemoveAllVeh", -1)
@@ -81,6 +155,45 @@ end)
 RegisterServerEvent("spawnVehAll")
 AddEventHandler("spawnVehAll", function()
 	TriggerClientEvent("SpawnAllVeh", -1)
+end)
+
+
+
+--RegisterServerEvent("SavellPlayer")
+--AddEventHandler("SavellPlayer", function(source)
+--	local _source = source
+--	local xPlayer = ESX.GetPlayerFromId(_source)
+--	--ESX.SavePlayers(cb)
+--	ESX.SavePlayer(xPlayer, cb)
+--	print('^2Save de '..xPlayer..' ^3Effectué')
+--	--for i=1, #xPlayers, 1 do
+--	--	local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
+--	--	--TriggerClientEvent('esx:showNotification', xPlayers[i], '✅ Synchronisation inventaire effectuée.')
+--	--end
+--
+--
+--end)
+
+
+RegisterServerEvent("SavellPlayerAuto")
+AddEventHandler("SavellPlayerAuto", function()
+	ESX.SavePlayers(cb)
+	print('^2Save des joueurs ^3Effectué')
+end)
+
+
+count = 0
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(1000)
+		count = count + 1
+
+		if count >= 240 then
+			ESX.SavePlayers(cb)
+			print('^2Save des joueurs ^3Effectué')
+			count = 0
+		end
+	end
 end)
 
 -----------------------------
@@ -130,7 +243,7 @@ function SessionBanPlayer(id, steam, source, type)
 	DropPlayer(id, SessionBanMsg)
 end
 
-local webhook = "https://discord.com/api/webhooks/802623021110132756/5q0ElqXCR3awRsOWe_MaIE7RvqyCVKWAnwuRtHY66qWFalFc5gK6F6JkylgxPxPqTFwG"
+local webhook = ""
 function WarnsLog(IdWarned, IdSource, Reason, banned)
 	if not banned then
 		message = GetPlayerName(IdSource).." à warn "..GetPlayerName(IdWarned).." pour "..Reason
@@ -198,7 +311,7 @@ AddEventHandler('h4ci_report:ajoutreport', function(typereport, sonid, reporteur
         ['@reporteur'] = reporteur,
         ['@nomreporter'] = nomreporter,
         ['@raison'] = raison
-	})	
+    })
 end)
 
 RegisterServerEvent('h4ci_report:supprimereport')
@@ -207,140 +320,3 @@ AddEventHandler('h4ci_report:supprimereport', function(supprimer)
             ['@id'] = supprimer
     })
 end)
-
-
--------------------------------------------------------
-
-RegisterCommand("newsreport", function(source, args, rawCommand)
-    if source ~= 0 then
-        local xPlayer = ESX.GetPlayerFromId(source)
-            if args[1] then
-                local message = string.sub(rawCommand, 3)
-                local xAll = ESX.GetPlayers()
-                for i=1, #xAll, 1 do
-                    local xTarget = ESX.GetPlayerFromId(xAll[i])
-                        TriggerClientEvent('chatMessage', xTarget.source, _U('newsreport', xPlayer.getName(), message))
-                end
-            else
-                TriggerClientEvent('chatMessage', xPlayer.source, _U('invalid_input', 'AdminChat'))
-            end
-    end
-end, false)
-
-RegisterCommand("staffservice", function(source, args, rawCommand)
-	if source ~= 0 then
-		local xPlayer = ESX.GetPlayerFromId(source)
-		if havePermission(xPlayer) then
-			if args[1] then
-				local message = string.sub(rawCommand, 3)
-				local xAll = ESX.GetPlayers()
-				for i=1, #xAll, 1 do
-					local xTarget = ESX.GetPlayerFromId(xAll[i])
-					if havePermission(xTarget) then
-						TriggerClientEvent('chatMessage', xTarget.source, _U('staffservice', xPlayer.getName(), xPlayer.getGroup(), message))
-					end
-				end
-			else
-				TriggerClientEvent('chatMessage', xPlayer.source, _U('invalid_inputt', 'AdminChat'))
-			end
-		end
-	end
-end, false)
-
-msg = ""
-
-RegisterCommand('alert', function(source, args, user)
-	if (IsPlayerAceAllowed(source, "command")) then
-			for i,v in pairs(args) do
-				msg = msg .. " " .. v
-			end
-			TriggerClientEvent("alert", -1, msg)
-			msg = ""
-    end
-end)
-
-Locales['en'] = {
-['newsreport']        = '\n^1Admin - Nouveau Report',
-['invalid_input']        = '\n^1Admin - Nouveau Report',
-['invalid_inputt']        = '\n^1Mode modération actif pour ^1[^4%s | Rang : %s^1]^0',
-['staffservice']        = '\n^1Mode modération activé pour ^1[^4%s | Rang : %s^1]^0'
-}
-
-
------------- functions and events ------------+
-
-RegisterNetEvent('esx:onPlayerDeath')
-AddEventHandler('esx:onPlayerDeath', function(data)
-	deadPlayers[source] = data
-end)
-
-RegisterNetEvent('esx:onPlayerSpawn')
-AddEventHandler('esx:onPlayerSpawn', function()
-	if deadPlayers[source] then
-		deadPlayers[source] = nil
-	end
-end)
-
-AddEventHandler('esx:playerDropped', function(playerId, reason)
-	-- empty tables when player no longer online
-	if onTimer[playerId] then
-		onTimer[playerId] = nil
-	end
-    if savedCoords[playerId] then
-    	savedCoords[playerId] = nil
-    end
-	if warnedPlayers[playerId] then
-		warnedPlayers[playerId] = nil
-	end
-	if deadPlayers[playerId] then
-		deadPlayers[playerId] = nil
-	end
-end)
-
-function havePermission(xPlayer, exclude)	-- you can exclude rank(s) from having permission to specific commands 	[exclude only take tables]
-	if exclude and type(exclude) ~= 'table' then exclude = nil; end	-- will prevent from errors if you pass wrong argument
-
-	local playerGroup = xPlayer.getGroup()
-	for k,v in pairs(Config.adminRanks) do
-		if v == playerGroup then
-			if not exclude then
-				return true
-			else
-				for a,b in pairs(exclude) do
-					if b == v then
-						return false
-					end
-				end
-				return true
-			end
-		end
-	end
-	return false
-end
-
------------------------
-
-
-RegisterServerEvent('shop:vehicule')
-AddEventHandler('shop:vehicule', function(vehicleProps, plate_625)
-    local xPlayer = ESX.GetPlayerFromId(source)
-    MySQL.Async.execute('INSERT INTO owned_vehicles (owner, plate, vehicle) VALUES (@owner, @plate, @vehicle)', {
-        ['@owner']   = xPlayer.identifier,
-        ['@plate']   = plate_625,
-        ['@vehicle'] = json.encode(vehicleProps)
-    }, function(rowsChange)
-    end)
-end)
-
----------------------------
-
-function webh(p)
-    local xPlayer = ESX.GetPlayerFromId(source)
-    local name = GetPlayerName(source)
-	local ep = GetPlayerEP(source)
-    local text = "Le staff : "..name.." c'est give de l'argent !"
-    PerformHttpRequest(discord_webhook.url, 
-    function(err, text, header) end, 
-    'POST', 
-    json.encode({username = serveur_name, content = text }), {['Content-Type'] = 'application/json'})
-end
