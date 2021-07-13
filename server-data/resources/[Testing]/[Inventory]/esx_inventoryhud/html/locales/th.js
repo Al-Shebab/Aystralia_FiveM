a28d967583ab3339891953e883aa358025d043b0
Mala <83369389+OfficialMala@users.noreply.github.com>
2021-07-13 12:59:03 +0300

    New inventory

--- server-data/resources/[Testing]/[Inventory]/esx_inventoryhud/html/locales/th.js
+++
@@ -1,9 +0,0 @@
-var locale = new Object();
-locale.dropItem = "<span class=\"emoji\">🗑️</span> ทิ้ง";
-locale.useItem = "<span class=\"emoji\">🖐️</span> ใช้";
-locale.giveItem = "<span class=\"emoji\">🎁</span> ให้";
-locale.getNumber = "ใส่จำนวน";
-locale.getPlayer = "เลือกผู้เล่น";
-locale.confirmNumber = "ตกลง";
-locale.closeInventory = "ปิด";
-locale.searchInventory = "ค้นหา";
