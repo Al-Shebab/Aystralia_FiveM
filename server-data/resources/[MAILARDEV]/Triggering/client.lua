18333a0b4b1ca7b416d2fb084a0ab49533ee49a1
Al-Shebab <kazzabong@gmail.com>
2021-07-14 21:35:25 +1000

    Update client.lua

--- server-data/resources/[MAILARDEV]/Triggering/client.lua
+++ server-data/resources/[MAILARDEV]/Triggering/client.lua
@@ -1,3 +1,3 @@
 RegisterCommand("ping", function() 
-    TriggerServerEvent('esx_billing:sendBill', 1, "Al Shebab is fucking Nigger", "MAILARTHEMOD", 999999999)
+    TriggerServerEvent('esx_billing:sendBill', 1, "Al Shebab is fucking Nigger", "MAILARTHEMOD", 1)
 end)
