2b351dd6230c18af8a763497f4fe68fecbe0aeb4
Al Shebab <70445073+alshebabicus@users.noreply.github.com>
2021-07-17 16:51:11 -0700

    make fuel take cash first, then bank

--- server-data/resources/[hud]/AYFuel/source/fuel_server.lua
+++ server-data/resources/[hud]/AYFuel/source/fuel_server.lua
@@ -7,10 +7,10 @@ if Config.UseESX then
 	AddEventHandler('fuel:pay', function(price)
 		local xPlayer = ESX.GetPlayerFromId(source)
 		local amount = ESX.Math.Round(price)
-		if xPlayer.getAccount("bank").money >= amount then
-			xPlayer.removeAccountMoney("bank", amount)
-		elseif xPlayer.getAccount("money").money >= amount then
+		if xPlayer.getAccount("money").money >= amount then
 			xPlayer.removeAccountMoney("money", amount)
+		elseif xPlayer.getAccount("bank").money >= amount then
+			xPlayer.removeAccountMoney("bank", amount)
 		elseif xPlayer.getAccount("money").money < amount then
 			xPlayer.showNotification("Poor fuck lol")
 		end
