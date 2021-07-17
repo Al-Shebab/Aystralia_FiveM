2b96bac8bfa4a3461533c0c152678262b2bc475b
Al Shebab <70445073+alshebabicus@users.noreply.github.com>
2021-07-17 11:38:37 -0700

    organise files

---
+++ server-data/resources/[jobs]/AYJobs/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'adamant'
+game 'gta5'
+
+client_script {
+    "dock-job-client.lua",
+    "dock-job-config.lua"
+}
+
+server_script {
+    "dock-job-server.lua",
+    "dock-job-config.lua"
+}
