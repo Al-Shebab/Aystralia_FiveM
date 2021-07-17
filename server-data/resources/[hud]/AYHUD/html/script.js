89754942116e1cbe9deb32d201aa4f9ca3f2cb9c
Al Shebab <70445073+alshebabicus@users.noreply.github.com>
2021-07-16 21:44:35 -0700

    fix the fucking bank hud

--- server-data/resources/[hud]/AYHUD/html/script.js
+++ server-data/resources/[hud]/AYHUD/html/script.js
@@ -18,16 +18,6 @@ $(document).ready(function () {
           setAnzahle(event.data.black);
   $(".schwarzmoney").show();
   $("schwarzmoney").show();
-  					  $(".voice1").hide();
-					  $(".voice2").hide();
-					  $(".voice3").hide();
-					  $(".voice4").hide();
-					  $(".voicemuted").hide();
-					  $(".voice1black").hide();
-					  $(".voice2black").hide();
-					  $(".voice3black").hide();
-					  $(".voice4black").hide();
-					  					    $(".voice" + event.data.level + "black").show();
 
 
         };
@@ -70,88 +60,6 @@ $(document).ready(function () {
   $(".funkeblack").hide();
 
         };
-		if (event.data.action == "muted") {
-					if (event.data.muted == true) {
-					if(!blackmoney) {
-					  $(".voice1").hide();
-					  $(".voice2").hide();
-					  $(".voice3").hide();
-					  $(".voice4").hide();
-					  $(".voicemuted").show();
-					  $(".voice1black").hide();
-					  $(".voice2black").hide();
-					  $(".voice3black").hide();
-					  $(".voice4black").hide();
-					  $(".voicemutedblack").hide();
-					} else {
-                      $(".voice1").hide();
-					  $(".voice2").hide();
-					  $(".voice3").hide();
-					  $(".voice4").hide();
-					  $(".voicemuted").hide();
-					  $(".voice1black").hide();
-					  $(".voice2black").hide();
-					  $(".voice3black").hide();
-					  $(".voice4black").hide();
-					  $(".voicemutedblack").show();
-					}
-						
-					}
-										if (event.data.muted == false) {
-					  $(".voicemuted").hide();
-											  $(".voicemutedblack").hide();
-
-					}
-
-        };
-		if (event.data.action == "nomuted") {
-					  $(".voicemuted").hide();
-											  $(".voicemutedblack").hide();
-
-					
-
-        };
-
-		if (event.data.action == "setVoiceLevel") {
-				if(!blackmoney) {
-					  $(".voice1").hide();
-					  $(".voice2").hide();
-					  $(".voice3").hide();
-					  $(".voice4").hide();
-					  $(".voice1black").hide();
-					  $(".voice2black").hide();
-					  $(".voice3black").hide();
-					  $(".voice4black").hide();
-  $(".voice" + event.data.level).show();
-				} else {
-					  $(".voice1").hide();
-					  $(".voice2").hide();
-					  $(".voice3").hide();
-					  $(".voice4").hide();
-					  $(".voice1black").hide();
-					  $(".voice2black").hide();
-					  $(".voice3black").hide();
-					  $(".voice4black").hide();
-					    $(".voice" + event.data.level + "black").show();
-
-				}
-
-        };
-		if (event.data.action == "setVoiceTalking") {
-			if (event.data.val) {
-				$('#microphone-alt-solid').attr("src", 'https://cdn.discordapp.com/attachments/788240573382787114/835897308209479760/microphone-alt-solid.png');
-			} else {
-				$('#microphone-alt-solid').attr("src", 'https://cdn.discordapp.com/attachments/812841412626087956/835908384183025694/14.png');
-			}
-		};
-
-		if (event.data.action == "setFunkTalking") {
-			if (event.data.val) {
-				$('#broadcast-tower-solid_1').attr("src", 'https://cdn.discordapp.com/attachments/788240573382787114/835897251531718716/broadcast-tower-solid_1.png');
-			} else {
-				$('#broadcast-tower-solid_1').attr("src", 'https://cdn.discordapp.com/attachments/788240573382787114/835908258714615848/13.png');
-			}
-		};
         if (event.data.action == "hideBlackMoney") {
   $(".schwarzmoney").hide();
   $("schwarzmoney").hide();
@@ -172,9 +80,9 @@ function setAnzahl(anzahl) {
 
 }
 function setAnzahle(anzahl) {
-	if (anzahl < 1) {
-		$('.schwarzmoney').hide();
-		$('#Ebene_3').hide();
+	if (anzahl < -1) {
+		$('.schwarzmoney').show();
+		$('#Ebene_3').show();
 		return;
 	} else {
 		$('.schwarzmoney').show();
