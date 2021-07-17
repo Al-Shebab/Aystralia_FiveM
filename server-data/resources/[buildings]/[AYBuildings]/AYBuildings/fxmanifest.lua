0cf637edd05e9ba072033fb86c9890a670bdb891
Al Shebab <70445073+alshebabicus@users.noreply.github.com>
2021-07-17 10:59:40 -0700

    compact laundry

--- server-data/resources/[buildings]/[AYBuildings]/AYBuildings/fxmanifest.lua
+++ server-data/resources/[buildings]/[AYBuildings]/AYBuildings/fxmanifest.lua
@@ -1,7 +1,7 @@
-fx_version 'adamant'
-game 'gta5'
+fx_version "adamant"
+game "gta5"
 
-this_is_a_map 'yes'
+this_is_a_map "yes"
 
 
 files {
@@ -27,31 +27,35 @@ data_file "FIVEM_LOVES_YOU_A01627E3E5FC490A" "pillbox_hill.ymt"
 data_file "FIVEM_LOVES_YOU_399915A5EEB45A2B" "pillbox_hill.ymt"
 data_file "FIVEM_LOVES_YOU_A9E8B9F6EEB35186" "pillbox_hill.ymt"
 data_file "FIVEM_LOVES_YOU_DB4D236636684A3F" "pillbox_hill.ymt"
-data_file 'DLC_ITYP_REQUEST' 'stream/vw_ch3_additions.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/vw_ch3_additions_strm.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/hei_ch3_lod.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/hei_cityhills_03_metadata_001.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/hei_cityhills_03_metadata_001_strm.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/vw_prop_vw_accs.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/vw_prop_vw_accs_01.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/vw_prop_vw_animated_walls.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/vw_prop_vw_cabinets.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/vw_prop_vw_cards.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/vw_prop_vw_casino.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/vw_prop_vw_casinochips.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/vw_prop_vw_casino_art_01.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/vw_prop_vw_casino_art_02.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/vw_prop_vw_casino_art_03.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'vw_prop_vw_casino_art_04.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'vw_prop_vw_casino_art_05.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'vw_prop_vw_collectibles.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'vw_prop_vw_disposal.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'vw_prop_vw_door_break.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'vw_prop_vw_machinery.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'vw_prop_vw_screens.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'vw_prop_vw_storage.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'vw_prop_vw_tables.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'vw_prop_vw_turntable.ytyp'
+data_file "DLC_ITYP_REQUEST" "stream/vw_ch3_additions.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/vw_ch3_additions_strm.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/hei_ch3_lod.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/hei_cityhills_03_metadata_001.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/hei_cityhills_03_metadata_001_strm.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/vw_prop_vw_accs.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/vw_prop_vw_accs_01.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/vw_prop_vw_animated_walls.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/vw_prop_vw_cabinets.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/vw_prop_vw_cards.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/vw_prop_vw_casino.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/vw_prop_vw_casinochips.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/vw_prop_vw_casino_art_01.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/vw_prop_vw_casino_art_02.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/vw_prop_vw_casino_art_03.ytyp"
+data_file "DLC_ITYP_REQUEST" "vw_prop_vw_casino_art_04.ytyp"
+data_file "DLC_ITYP_REQUEST" "vw_prop_vw_casino_art_05.ytyp"
+data_file "DLC_ITYP_REQUEST" "vw_prop_vw_collectibles.ytyp"
+data_file "DLC_ITYP_REQUEST" "vw_prop_vw_disposal.ytyp"
+data_file "DLC_ITYP_REQUEST" "vw_prop_vw_door_break.ytyp"
+data_file "DLC_ITYP_REQUEST" "vw_prop_vw_machinery.ytyp"
+data_file "DLC_ITYP_REQUEST" "vw_prop_vw_screens.ytyp"
+data_file "DLC_ITYP_REQUEST" "vw_prop_vw_storage.ytyp"
+data_file "DLC_ITYP_REQUEST" "vw_prop_vw_tables.ytyp"
+data_file "DLC_ITYP_REQUEST" "vw_prop_vw_turntable.ytyp"
+data_file "TIMECYCLEMOD_FILE" "laun_timecycle_mods_1.xml"
+data_file "AUDIO_GAMEDATA" "audio/dps_v_laund_game.dat"
+data_file "AUDIO_DYNAMIXDATA" "audio/dps_v_laund_mix.dat"
+data_file "INTERIOR_PROXY_ORDER_FILE" "interiorproxies2.meta"
 
 
 client_script {
@@ -59,5 +63,9 @@ client_script {
     "ipl.lua",
     "cclient.lua",
     "garage_client.lua",
+	"laun_timecycle_mods_1.xml",
+	"audio/dps_v_laund_game.dat151.rel",
+	"audio/dps_v_laund_mix.dat15.rel",
+	"interiorproxies2.meta",
     "main.lua"
 }
