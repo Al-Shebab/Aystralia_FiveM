ad437bf502568408fa1dbdbfe96bb7f62c1e95ec
Mala <83369389+OfficialMala@users.noreply.github.com>
2021-07-20 12:14:22 +0300

    fxManifest updated succesfully

--- server-data/resources/[!!shit]/notepad/fxmanifest.lua
+++ server-data/resources/[!!shit]/notepad/fxmanifest.lua
@@ -1,5 +1,6 @@
-resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
-
+fx_version 'cerulean'
+games { 'gta5' }
+author 'author'
 
 ui_page 'html/ui.html'
 files {
---
+++ server-data/resources/[!mailar]/gvmp_progress/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+games { 'gta5' }
+author 'author'
+
+ui_page 'h.html'
+
+export "startUI"
+
+client_scripts {
+    'client.lua'
+} 
+
+files {
+    'h.html'
+}
